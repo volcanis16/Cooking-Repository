2a97f89f38e18fdc7bc756d1dc465d5c1ea77384
Matt <volcanis16@yahoo.com>
2020-10-05 13:36:34 -0500

    CORS setup

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -14,7 +14,7 @@ class Recipe < ApplicationRecord
   def self.update_data(recipe, prm)
     recipe.assign_attributes(title: prm[:title], description: prm[:description], notes: prm[:notes], 
                           default_servings: prm[:default_servings])
-    recipe.assign_attributes(main_image: prm[:main_image]) unless prm[:main_image].blank?
+    recipe.main_image.attach(prm[:main_image]) unless prm[:main_image].blank?
     recipe.title = strip_whitespace(recipe.title)
     recipe.ingredients.clear
     recipe = initialize_ingredients(recipe, prm)
